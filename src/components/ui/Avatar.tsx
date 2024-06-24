306611f254ee7c267657d0af623c37bf850ff114
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-06-24 16:58:40 +0530

    Avatar component

--- src/components/ui/Avatar.tsx
+++ src/components/ui/Avatar.tsx
@@ -8,23 +8,23 @@ interface AvatarProps {
 }
 
 const Avatar : React.FC<AvatarProps>= ({src , background ,icon , profile}) => {
-  return (
-    <div className={`rounded-full w-10 h-10 ${background} text-white flex items-center justify-center`}>
+    return (
+        <div className={`rounded-full w-10 h-10 ${background} text-white flex items-center justify-center`}>
         
-        {icon && (
-            <div className=" flex items-center justify-center w-5 h-5 rotate-90">
-            {icon}
+            {icon && (
+                <div className=" flex items-center justify-center w-5 h-5 rotate-90">
+                    {icon}
+                </div>
+            )}
+
+            {profile && (
+                <p className="text-xl font-bold text-black flex items-center justify-center w-5 h-5">
+                    {profile}
+                </p>
+            )}
+
         </div>
-        )}
-
-        {profile && (
-            <p className="text-xl font-bold text-black flex items-center justify-center w-5 h-5">
-            {profile}
-        </p>
-        )}
-
-    </div>
-  )
+    )
 }
 
 export default Avatar

06db455f8156b37d2f9d053e7ed6c31aaab943b8
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-06-24 16:56:01 +0530

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link'
+import React from 'react'
+import Avatar from './ui/Avatar'
+import { Search } from 'lucide-react'
+
+const Navbar = () => {
+    return (
+        <div className="absolute top-5 left-1/2 transform -translate-x-1/2 w-[40vw] h-[4vw] rounded-lg bg-stone-200 border border-stone-300 flex items-center justify-between px-6">
+            
+            <div className="flex flex-row justify-center items-center gap-x-7">
+                <Link href="#"><h2 className='font-extrabold text-[2.4rem]'>BB.</h2></Link>
+                <Link href="#" className='underline'>Blog your Story</Link>
+            </div>
+
+            <div className="flex flex-row justify-center items-center gap-x-2">
+                <Avatar 
+                    background={"bg-black"}
+                    icon={<Search />}
+                />
+                <Avatar 
+                    background={"bg-orange-600"}
+                    profile="A"
+                />
+            </div>
+
+        </div>
+    )
+}
+
+export default Navbar
---
+++ src/components/ui/Avatar.tsx
@@ -0,0 +1,30 @@
+import { profile } from "console"
+
+interface AvatarProps {
+    src?:string
+    background:string
+    icon?:string | JSX.Element 
+    profile?:string
+}
+
+const Avatar : React.FC<AvatarProps>= ({src , background ,icon , profile}) => {
+  return (
+    <div className={`rounded-full w-10 h-10 ${background} text-white flex items-center justify-center`}>
+        
+        {icon && (
+            <div className=" flex items-center justify-center w-5 h-5 rotate-90">
+            {icon}
+        </div>
+        )}
+
+        {profile && (
+            <p className="text-xl font-bold text-black flex items-center justify-center w-5 h-5">
+            {profile}
+        </p>
+        )}
+
+    </div>
+  )
+}
+
+export default Avatar

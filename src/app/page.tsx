0d445ffe09c11405b9459a961e003c1e74125c1d
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-06-25 20:56:45 +0530

    page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import CardSection from "@/components/CardSection"
+import Navbar from "@/components/Navbar"
 
 
 const HomePage = () => {
   return (
-    <div className="h-screen w-full flex items-center">
+    <div className="relative h-screen w-full bg-white">
+      <Navbar />
       <CardSection />
     </div>
+      
   )
 }
 

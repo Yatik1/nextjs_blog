9462a32cf8cde19bb12988f1200dc13f113135f2
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-06-25 20:56:54 +0530

    debug

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ import { Search } from 'lucide-react'
 
 const Navbar = () => {
     return (
-        <div className="absolute top-5 left-1/2 transform -translate-x-1/2 w-[40vw] h-[4vw] rounded-lg bg-stone-200 border border-stone-300 flex items-center justify-between px-6">
+        <div className="sticky z-[6] top-5 left-1/2 transform -translate-x-1/2 w-[40vw] h-[4vw] rounded-lg bg-stone-200 border border-stone-300 flex items-center justify-between px-6">
             
             <div className="flex flex-row justify-center items-center gap-x-7">
                 <Link href="#"><h2 className='font-extrabold text-[2.4rem]'>BB.</h2></Link>
@@ -15,12 +15,12 @@ const Navbar = () => {
             <div className="flex flex-row justify-center items-center gap-x-2">
                 
                 <Avatar 
-                    background={"bg-black"}
+                    styles={"bg-black"}
                     icon={<Search />}
                 />
 
                 <Avatar 
-                    background={"bg-orange-600"}
+                    styles={"bg-orange-600"}
                     profile="A"
                 />
                 

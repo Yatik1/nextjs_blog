a894239ff56582e6920d47a3412762b7050ffd4c
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-06-25 17:13:52 +0530

    section

---
+++ src/components/CardSection.tsx
@@ -0,0 +1,41 @@
+import Card from './ui/Card'
+
+const CardSection = () => {
+  return (
+    <div className="mt-9 pl-[4rem] py-[4rem] flex flex-row gap-x-10">
+        <Card 
+            title="Welcome to the blog"
+            cover="https://images.unsplash.com/photo-1719257726822-9899127bb072?q=80&w=1410&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            author="Yatik"
+        />
+        <Card 
+            title="Welcome to the blog"
+            cover="https://images.unsplash.com/photo-1719257726822-9899127bb072?q=80&w=1410&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            author="Yatik"
+        />
+        
+        <Card 
+            title="Welcome to the blog"
+            cover="https://images.unsplash.com/photo-1719257726822-9899127bb072?q=80&w=1410&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            author="Yatik"
+        />
+        <Card 
+            title="Welcome to the blog"
+            cover="https://images.unsplash.com/photo-1719257726822-9899127bb072?q=80&w=1410&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            author="Yatik"
+        />
+        <Card 
+            title="Welcome to the blog"
+            cover="https://images.unsplash.com/photo-1719257726822-9899127bb072?q=80&w=1410&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            author="Yatik"
+        />
+        <Card 
+            title="Welcome to the blog"
+            cover="https://images.unsplash.com/photo-1719257726822-9899127bb072?q=80&w=1410&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            author="Yatik"
+        />
+    </div>
+  )
+}
+
+export default CardSection

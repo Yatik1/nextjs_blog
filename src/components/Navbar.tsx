7093a1143d3a016c4b37382a9afe80695e5ee8ab
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-06-28 17:23:47 +0530

    debug

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,11 +5,11 @@ import { Search } from 'lucide-react'
 
 const Navbar = () => {
     return (
-        <div className="sticky z-[6] top-5 left-1/2 transform -translate-x-1/2 w-[40vw] h-[4vw] rounded-lg bg-stone-200 border border-stone-300 flex items-center justify-between px-6">
+        <div className="absolute z-[1] top-5 left-1/2 transform -translate-x-1/2 w-[40vw] h-[4vw] rounded-lg bg-stone-200 border border-stone-300 flex items-center justify-between px-6">
             
             <div className="flex flex-row justify-center items-center gap-x-7">
-                <Link href="#"><h2 className='font-extrabold text-[2.4rem]'>BB.</h2></Link>
-                <Link href="#" className='underline'>Blog your Story</Link>
+                <Link href="/"><h2 className='font-extrabold text-[2.4rem]'>BB.</h2></Link>
+                <Link href="/write" className='underline'>Blog your Story</Link>
             </div>
 
             <div className="flex flex-row justify-center items-center gap-x-2">

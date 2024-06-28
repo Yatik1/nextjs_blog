e4cd96475af45f2584546506e990a06af06dba85
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-06-28 17:23:52 +0530

    avatar

--- src/components/ui/Avatar.tsx
+++ src/components/ui/Avatar.tsx
@@ -1,29 +1,29 @@
+import React from "react";
+
 interface AvatarProps {
-    src?:string
-    styles:string
-    icon?:string | JSX.Element 
-    profile?:string
-    ref?:React.RefObject<HTMLDivElement> | React.MutableRefObject<HTMLDivElement>
+  src?: string;
+  styles: string;
+  icon?: string | JSX.Element;
+  profile?: string;
 }
 
-const Avatar : React.FC<AvatarProps>= ({src , styles ,icon , profile,ref}) => {
-    return (
-        <div ref={ref} className={`cursor-pointer rounded-full w-10 h-10 ${styles} text-white flex items-center justify-center`}>
-        
-            {icon && (
-                <div className=" flex items-center justify-center w-5 h-5">
-                    {icon}
-                </div>
-            )}
-
-            {profile && (
-                <p className="text-xl font-bold text-black flex items-center justify-center w-5 h-5">
-                    {profile}
-                </p>
-            )}
-
+// Using React.forwardRef to handle the ref
+const Avatar = React.forwardRef<HTMLDivElement, AvatarProps>((props, ref) => {
+  const { src, styles, icon, profile } = props;
+  return (
+    <div ref={ref} className={`cursor-pointer rounded-full w-10 h-10 ${styles} text-white flex items-center justify-center`}>
+      {icon && (
+        <div className="flex items-center justify-center w-5 h-5">
+          {icon}
         </div>
-    )
-}
+      )}
+      {profile && (
+        <p className="text-xl font-bold text-black flex items-center justify-center w-5 h-5">
+          {profile}
+        </p>
+      )}
+    </div>
+  );
+});
 
-export default Avatar
+export default Avatar;

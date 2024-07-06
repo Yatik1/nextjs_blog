916d766b8fc091c7bb3b6438e08564b7ce74e48d
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-07-06 13:41:56 +0530

    Card Section

--- src/components/CardSection.tsx
+++ src/components/CardSection.tsx
@@ -1,55 +1,53 @@
+"use client"
+
+import { useEffect, useState } from 'react'
 import Card from './ui/Card'
+import { Blogs } from '@/types/types'
+import { getAllBlogs } from '@/actions/getDataAll'
 
 const CardSection = () => {
+
+    const [blogs,setBlogs] = useState<Blogs[]>([])
+    const [loading,setLoading] = useState<boolean>(false)
+    
+    useEffect(() => {
+        const fetchBlogs = async () => {
+            try {
+                setLoading(true)
+
+                const data = await getAllBlogs()
+                setBlogs(data)
+
+            } catch (error) {
+                console.log(error)
+            } finally {
+                setLoading(false)
+            }
+        }
+
+        fetchBlogs()
+
+    } , [])
+
     return (
         <div className="absolute top-[14%] h-full w-auto flex items-start pl-7">
-            {[1,2,3,4,5,6].map((item,index) => (
-                <Card 
-                    title="Welcome to the blog"
-                    cover="https://images.unsplash.com/photo-1719257726822-9899127bb072?q=80&w=1410&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                    author="Yatik"
-                    index={index}
-                />  
-            ))}          
+            {blogs.length === 0 ? 
+                (
+                    <p className='mt-[10rem] flex justify-center items-start w-screen h-screen text-slate-500'>No Blogs Available. ❌</p>
+                ) :
+                (
+                    blogs.map((data,index) => (
+                        <Card 
+                            id={data._id}
+                            title={data.title}
+                            cover={data.coverImg}
+                            index={index}
+                        />  
+                    ))
+                )
+            }          
         </div>
     )
 }
 
 export default CardSection
-
-      
-// <div className="mt-9 pl-[4rem] py-[4rem] flex flex-row gap-x-10 bg-red-400 w-full">
-// <Card />
-// </div>
-
-// <Card 
-//             title="Welcome to the blog"
-//             cover="https://images.unsplash.com/photo-1719257726822-9899127bb072?q=80&w=1410&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-//             author="Yatik"
-//         />
-//         <Card 
-//             title="Welcome to the blog"
-//             cover="https://images.unsplash.com/photo-1719257726822-9899127bb072?q=80&w=1410&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-//             author="Yatik"
-//         />
-        
-//         <Card 
-//             title="Welcome to the blog"
-//             cover="https://images.unsplash.com/photo-1719257726822-9899127bb072?q=80&w=1410&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-//             author="Yatik"
-//         />
-//         <Card 
-//             title="Welcome to the blog"
-//             cover="https://images.unsplash.com/photo-1719257726822-9899127bb072?q=80&w=1410&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-//             author="Yatik"
-//         />
-//         <Card 
-//             title="Welcome to the blog"
-//             cover="https://images.unsplash.com/photo-1719257726822-9899127bb072?q=80&w=1410&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-//             author="Yatik"
-//         />
-//         <Card 
-//             title="Welcome to the blog"
-//             cover="https://images.unsplash.com/photo-1719257726822-9899127bb072?q=80&w=1410&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-//             author="Yatik"
-//         />

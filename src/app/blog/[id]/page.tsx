2aa96b760200c95e94b9b4ffa0479cbc4a23c95d
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-07-07 13:50:57 +0530

    page

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,11 +1,39 @@
 "use client"
 
+import { Blogs } from '@/types/types'
+import axios from 'axios'
 import { useParams } from 'next/navigation'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const BlogPage = () => {
-    const params = useParams()
-    const {id} = params
+
+const BlogPage : React.FC = () => {
+
+    const params = useParams();
+    const {id} = params as {id:string}
+
+    const [data , setData] = useState<Blogs | null>(null)
+    const [loading , setLoading] = useState<boolean>(false)
+
+    useEffect(() => {
+      const fetchBlogById = async (id : string) => {
+          try {
+            
+            setLoading(true)
+
+            const response = await axios.get<Blogs>(`/api/blog/${id}`)
+            setData(response.data)
+
+          } catch (error) {
+            console.log(error)
+          } finally {
+            setLoading(false)
+          }
+      }
+
+      fetchBlogById(id)
+    } , [id])
+
+    console.log(data)
 
   return (
     <div>This is Blog Page of Blog id : {id} </div>

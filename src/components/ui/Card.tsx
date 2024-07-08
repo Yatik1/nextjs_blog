4a72f858e452733e49acd3083cb023a433c04687
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-07-08 13:25:46 +0530

    cards

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -19,7 +19,7 @@ const  Card : React.FC<CardProps>= ({title,author,cover,index , id}) => {
 
   const linkRef = useRef<HTMLDivElement>(null)
   const router = useRouter()
-  const isEven = index % 2 === 1
+  const isThird = (index + 1) % 3 === 0
 
   function handleClick() {
     router.push(`/blog/${id}`)
@@ -27,30 +27,31 @@ const  Card : React.FC<CardProps>= ({title,author,cover,index , id}) => {
 
   return(
     <div key={index} className="flex flex-col gap-5 items-start h-full w-auto px-[1vw]">
+
       <div 
-        className={`relative bg-stone-300 ${isEven ? 'w-[42vw] h-[30vw]' : 'w-[21vw] h-[30vw]'} rounded-[2rem] overflow-hidden mt-[6rem]`}
+        className={`relative bg-stone-300 ${isThird ? 'w-[42vw] h-[30vw]' : 'w-[21vw] h-[30vw]'} rounded-[2rem] overflow-hidden mt-[3rem]`}
         onMouseEnter={() => {console.log(linkRef)}}
       >
           
           <Image 
             src={cover}
             alt="image"
-            className="absolute inset-0 w-full h-full object-cover"
+            className="absolute inset-0 w-full h-full"
             layout="fill"
             objectFit="cover"
           />
 
           <Avatar 
             ref={linkRef}
-            styles="absolute right-2 top-2 bg-black"
+            styles="absolute right-2 top-2 bg-stone-800 hover:bg-stone-100 hover:text-black"
             icon={<ArrowUpRight/>}      
             onclick= {handleClick}
           />
 
       </div>
       
-      <div className="flex flex-col leading-3 w-full p-2">
-        <h2 className="text-2xl font-bold">{title}</h2>
+      <div className="flex flex-col leading-3 bg-purple-50 p-2">
+        <h2 className="text-xl font-semibold tracking-tighter">{title}</h2>
         <p className="text-[1rem] underline italic ">{author}</p>
       </div>
 

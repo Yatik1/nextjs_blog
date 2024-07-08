4bc168298cffcd69454bf7c3b72b4eb351c7dc00
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-07-08 13:25:06 +0530

    root page

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -4,6 +4,11 @@ import CardSection from "@/components/CardSection"
 const HomePage = () => {
   return (
     <div className="relative h-screen w-full bg-white">
+
+      <div className="flex items-center justify-center">
+        <h1 className="font-bold italic text-[3rem] mt-[4rem] pt-[3rem] ">All Blogs{"->"}</h1>
+      </div>
+
       <CardSection />
     </div>
       

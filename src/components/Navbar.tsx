e6cc6eb5324cd0deda04feaa4b41cde2d0e84368
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-07-09 13:38:39 +0530

    nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,11 @@
 import Link from 'next/link'
-import React from 'react'
 import Avatar from './ui/Avatar'
 import { Search } from 'lucide-react'
 
 const Navbar = () => {
+
     return (
-        <div className="absolute z-[1] top-5 left-1/2 transform -translate-x-1/2 w-[40vw] h-[4vw] rounded-lg bg-stone-200 border border-stone-300 flex items-center justify-between px-6">
+        <div className="fixed z-[1] top-5 left-1/2 transform -translate-x-1/2 w-[40vw] h-[4vw] rounded-lg bg-stone-200 border border-stone-300 flex items-center justify-between px-6">
             
             <div className="flex flex-row justify-center items-center gap-x-7">
                 <Link href="/"><h2 className='font-extrabold text-[2.4rem]'>BB.</h2></Link>

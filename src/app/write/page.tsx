f309d3a744816efcb0a2e0a2c5bf598102d5e969
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-07-11 13:44:38 +0530

    page

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -4,7 +4,8 @@ const Blog = () => {
   return (
     <div className="flex items-center justify-center absolute w-screen h-full mt-[1.6rem]">
         <div className="flex flex-col items-start justify-start w-11/12 p-7">
-            <h1 className="font-bold italic text-[4rem] underline ml-[28vw]">Write your blog.</h1>
+            <h1 className="font-bold italic text-[4rem] ml-[28vw]">Write your blog</h1>
+            <hr className=' w-full' />
             <FormEditor />
         </div>
     </div>

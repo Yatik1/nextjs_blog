3a484227d75067898280de37658fbb2f289d7561
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-07-12 14:10:55 +0530

    page

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,15 +1,32 @@
+"use client"
+
 import CardSection from "@/components/CardSection"
+import useBlog from "../hooks/useBlogModal"
+import { useEffect } from "react"
+import MobileCardSection from "@/components/MobileCardSection"
 
 
 const HomePage = () => {
+
+  const {blogs , loading , fetchBlogs} = useBlog()
+
+  useEffect(() => {
+    fetchBlogs()
+  } , [fetchBlogs])
+
+  if (loading) {
+    return <p className="flex w-full h-screen items-center justify-center">Loading ..... </p>
+  }
+
   return (
-    <div className="relative h-screen w-full bg-white">
+    <div className="relative h-screen w-full bg-white md:overflow-x-hidden">
 
       <div className="flex items-center justify-center">
-        <h1 className="font-bold italic text-[3rem] mt-[4rem] pt-[3rem] ">All Blogs{"->"}</h1>
+        <h1 className="font-bold italic text-[3rem] mt-[4rem] pt-[3rem]">All Blogs{"->"}</h1>
       </div>
 
-      <CardSection />
+      <CardSection blogs = {blogs}/>
+      <MobileCardSection blogs={blogs}/>
 
     </div>
       
@@ -17,10 +34,3 @@ const HomePage = () => {
 }
 
 export default HomePage
-
-
-
-{/* <div className="absolute bg-black flex flex-row gap-5">
-<div className="rounded-full w-10 h-10 bg-red-200 text-black flex items-center justify-center" onClick={() => alert("left")}>{`>`}</div>
-<div className="rounded-full w-10 h-10 bg-red-200 text-black flex items-center justify-center" onClick={}>{`<`}</div>
-</div> */}

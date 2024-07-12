d5443f5e259b69ed2c84cb8b5eb8cdc776e867e1
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-07-12 14:11:03 +0530

    cardSection

--- src/components/CardSection.tsx
+++ src/components/CardSection.tsx
@@ -5,31 +5,10 @@ import Card from './ui/Card'
 import { Blogs } from '@/types/types'
 import { getAllBlogs } from '@/actions/getDataAll'
 import {motion} from "framer-motion"
+import MobileCardSection from './MobileCardSection'
 
 
-const CardSection = () => {
-
-    const [blogs,setBlogs] = useState<Blogs[]>([])
-    const [loading,setLoading] = useState<boolean>(false)
-    
-    useEffect(() => {
-        const fetchBlogs = async () => {
-            try {
-                setLoading(true)
-
-                const data = await getAllBlogs()
-                setBlogs(data)
-
-            } catch (error) {
-                console.log(error)
-            } finally {
-                setLoading(false)
-            }
-        }
-
-        fetchBlogs()
-
-    } , [])
+const CardSection = ({blogs} : {blogs : Blogs[]}) => {
 
     return (
         <div className="absolute flex items-start pl-7 pb-[3rem]">
@@ -39,31 +18,32 @@ const CardSection = () => {
                     <p className='mt-[10rem] flex justify-center items-start w-screen h-screen text-slate-500'>No Blogs Available. ❌</p>
                 ) :
                 (
-                    <motion.div 
-                        initial={{ x: 0 }}
-                        animate={{ x: "-50%" }}
-                        transition={{ repeat: Infinity, ease: "linear", duration:30, delay:0.5 }}
-                        className="flex gap-[1.5rem] items-start"
-                    >
-                        {blogs.map((data,index) => (
-                            <Card 
-                                id={data._id}
-                                title={data.title}
-                                cover={data.coverImg}
-                                author='Yatik'
-                                index={index}
-                            />  
-                        ))}
-                        {blogs.map((data,index) => (
-                            <Card 
-                                id={data._id}
-                                title={data.title}
-                                cover={data.coverImg}
-                                author='Yatik'
-                                index={index}
-                            />  
-                        ))}
-                    </motion.div>
+                    <>
+                        <motion.div 
+                            initial={{ x: 0 }}
+                            animate={{ x: "-50%" }}
+                            transition={{ repeat: Infinity, ease: "linear", duration:30, delay:0.5 }}
+                            className="gap-[1.5rem] items-start hidden md:flex"
+                        >
+                            {blogs.map((data,index) => (
+                                <Card 
+                                    id={data._id}
+                                    title={data.title}
+                                    cover={data.coverImg}
+                                    author='Yatik'
+                                    index={index}
+                                />  
+                            ))}
+                            {blogs.map((data,index) => (
+                                <Card 
+                                    id={data._id}
+                                    title={data.title}
+                                    cover={data.coverImg}
+                                    index={index}
+                                />  
+                            ))}
+                        </motion.div>                        
+                    </>
                 )
             }          
         </div>

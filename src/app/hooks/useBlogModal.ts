b30525e85e15361aa84c57cb3eb5eabbe87034d2
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-07-12 14:10:50 +0530

    hook

---
+++ src/app/hooks/useBlogModal.ts
@@ -0,0 +1,26 @@
+import { Blogs } from '@/types/types'
+import axios from 'axios'
+import {create} from 'zustand'
+
+interface BlogsState {
+    blogs : Blogs[]
+    loading:boolean
+    fetchBlogs: () => Promise<void>
+}
+
+const useBlog = create<BlogsState>((set) => ({
+    blogs:[],
+    loading : false,
+    fetchBlogs : async () => {
+        set({loading : true})
+        try {
+            const response = await axios.get<Blogs[]>('/api/')
+            set({blogs : response.data})
+        } catch (error) {
+            console.log("Fetch error : ",error)
+        } finally {
+            set({loading : false})
+        }
+    }
+}))
+export default useBlog

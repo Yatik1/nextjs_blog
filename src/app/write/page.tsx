81bc50a29a6677514286028e7317257bb8495014
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-07-26 13:28:34 +0530

    write page

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -1,6 +1,15 @@
 import FormEditor from "@/components/FormEditor"
+import { auth } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation"
 
 const Blog = () => {
+
+  const {userId} = auth()
+
+  if(!userId) {
+    redirect("/sign-in")
+  } 
+
   return (
     <div className="flex items-center justify-center absolute w-screen h-full mt-[1.6rem]">
         <div className="flex flex-col items-start justify-start w-11/12 p-7">

3a367f3b17c85fb357b82906ec32fcf2800813b9
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-07-26 13:27:54 +0530

    Layout auth Provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import { ClerkProvider } from "@clerk/nextjs";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,11 +18,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <ClerkProvider>
+      <html lang="en">
       <body className={inter.className}>
       <Navbar />
         {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }

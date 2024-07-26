fc83072378e57bb1deff2e725d09a9701ee06e92
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-07-26 13:31:30 +0530

    :: Avatar

--- src/components/ui/Avatar.tsx
+++ src/components/ui/Avatar.tsx
@@ -11,14 +11,14 @@ interface AvatarProps {
 const Avatar = React.forwardRef<HTMLDivElement, AvatarProps>((props, ref) => {
   const { src, styles, icon, profile , onclick } = props;
   return (
-    <div ref={ref} onClick={onclick} className={`cursor-pointer rounded-full w-10 h-10 ${styles} text-white flex items-center justify-center`}>
+    <div ref={ref} onClick={onclick} className={`cursor-pointer rounded-full w-8 h-8 ${styles} text-white flex items-center justify-center`}>
       {icon && (
-        <div className="flex items-center justify-center w-5 h-5">
+        <div className="flex items-center justify-center w-3 h-3">
           {icon}
         </div>
       )}
       {profile && (
-        <p className="text-xl font-bold text-black flex items-center justify-center w-5 h-5">
+        <p className="text-xl font-semibold text-black flex items-center justify-center w-2 h-2">
           {profile}
         </p>
       )}

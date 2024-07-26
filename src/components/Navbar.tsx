02dc92ec325871c5bf9387a16704106c7dead847
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-07-26 13:28:40 +0530

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,22 @@
+"use client"
+
+import { useRouter } from 'next/navigation';
+import { useAuth, UserButton } from '@clerk/nextjs'
 import Link from 'next/link'
 import Avatar from './ui/Avatar'
-import { Search } from 'lucide-react'
+
 
 const Navbar = () => {
+
+    const {isSignedIn} = useAuth()
+    
+    const router = useRouter()
+    
+    function authHandler() {
+        router.push("/sign-in")
+    }
+
+
 
     return (
         <div className="fixed z-[8] top-5 left-1/2 transform -translate-x-1/2 lg:w-[40vw] w-[80vw] rounded-lg bg-stone-200 border border-stone-300 flex items-center justify-between px-6">
@@ -13,20 +27,15 @@ const Navbar = () => {
             </div>
 
             <div className="flex flex-row justify-center items-center gap-x-2">
-                
-                <Avatar 
-                    styles={"bg-black"}
-                    icon={<Search />}
-                />
-
-                <Avatar 
-                    styles={"bg-orange-600"}
-                    profile="A"
-                />
+                {
+                    isSignedIn ? 
+                    (  <UserButton afterSignOutUrl='/' />  ) : 
+                    (  <Avatar styles={"bg-orange-600"} profile="A" onclick={authHandler} /> ) 
+                }
                 
             </div>
 
-        </div>
+        </div> 
     )
 }
 

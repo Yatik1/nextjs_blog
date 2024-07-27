4160682c08f9d5334b804207060b6e17c20003d5
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-07-27 18:33:57 +0530

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,25 +12,34 @@ const Navbar = () => {
     
     const router = useRouter()
     
-    function authHandler() {
+    function loginHandler() {
         router.push("/sign-in")
+    }
+
+    function signupHandler() {
+        router.push("/sign-up")
     }
 
 
 
     return (
-        <div className="fixed z-[8] top-5 left-1/2 transform -translate-x-1/2 lg:w-[40vw] w-[80vw] rounded-lg bg-stone-200 border border-stone-300 flex items-center justify-between px-6">
+        <div className="w-full border-b border-stone-300 flex items-center justify-between md:px-[6rem] px-3 py-1">
             
             <div className="flex flex-row justify-center items-center gap-x-2 md:gap-x-7">
                 <Link href="/"><h2 className='font-extrabold text-[2.4rem]'>BB.</h2></Link>
-                <Link href="/write" className='underline tracking-tighter text-[0.75rem] md:text-sm'>Blog your Story</Link>
+                <Link href="/write" className='tracking-tighter text-[0.75rem] md:text-sm inline py-1 px-3  bg-black hover:bg-stone-700 rounded-md text-white cursor-pointer'>Write Blog</Link>
             </div>
 
             <div className="flex flex-row justify-center items-center gap-x-2">
                 {
                     isSignedIn ? 
                     (  <UserButton afterSignOutUrl='/' />  ) : 
-                    (  <Avatar styles={"bg-orange-600"} profile="A" onclick={authHandler} /> ) 
+                    (  
+                        <div className='flex justify-center items-center gap-2'>
+                            <button onClick={loginHandler} className='inline py-1 px-3  bg-black hover:bg-stone-700 rounded-md text-white cursor-pointer text-[0.75rem] md:text-sm '>Login</button>
+                            <button onClick={signupHandler} className='inline py-1 px-2 bg-gray-100 border border-stone-300 hover:bg-black rounded-md text-black hover:text-white cursor-pointer text-[0.75rem] md:text-sm '>Sign Up</button>
+                        </div>
+                    ) 
                 }
                 
             </div>

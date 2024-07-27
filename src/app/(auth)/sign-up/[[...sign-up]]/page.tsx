a44b39bb5bbe28c469f6358f0ce193416cb88d01
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-07-27 18:33:22 +0530

    sign-up

--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
     return (
-        <div className="w-screen h-screen bg-white absolute z-[9999] flex justify-center items-center">
+        <div className="w-full h-full bg-white absolute z-[9999] top-0 flex justify-center items-center">
             <SignUp />
         </div>
     )

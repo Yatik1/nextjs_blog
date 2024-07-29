365787716679f455736e561724720f006781c476
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-07-29 15:36:02 +0530

    Mobile interface card linkable

--- src/components/MobileCardSection.tsx
+++ src/components/MobileCardSection.tsx
@@ -1,6 +1,7 @@
 import { Blogs } from '@/types/types'
 import React from 'react'
 import Card from './ui/Card'
+import Link from 'next/link'
 
 const MobileCardSection = ({blogs} : {blogs : Blogs[]}) => {
 
@@ -8,12 +9,14 @@ const MobileCardSection = ({blogs} : {blogs : Blogs[]}) => {
     <div className="h-fit w-screen md:hidden">
         <div className='flex flex-col gap-2 justify-start items-center mb-3'>
         {blogs.map((data,index) => (
-            <Card
-            id={data._id}
-            title={data.title}
-            cover={data.coverImg}
-            index={index}
-        /> 
+            <Link href={`/blog/${data._id}`}>
+              <Card
+              id={data._id}
+              title={data.title}
+              cover={data.coverImg}
+              index={index}
+            /> 
+            </Link>
         ))}
     </div>
     </div>

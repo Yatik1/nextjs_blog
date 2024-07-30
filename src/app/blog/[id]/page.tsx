b89a6c8acb3d5e5320b0d860ba871ead1dceb59a
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-07-30 23:00:06 +0530

    adding authorized edit and delete feature

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -2,16 +2,27 @@
 
 import { Blogs } from '@/types/types'
 import { formatDate } from '@/utils/dateFormat'
+import { useAuth } from '@clerk/nextjs'
 import axios from 'axios'
+import { Edit, Trash } from 'lucide-react'
 import Image from 'next/image'
-import { useParams } from 'next/navigation'
+import { useParams, useRouter } from 'next/navigation'
 import React, { useEffect, useState } from 'react'
+import toast from 'react-hot-toast'
 
 
 const BlogPage : React.FC = () => {
 
     const params = useParams();
+    const router = useRouter();
+
     const {id} = params as {id:string}
+
+    const {userId} = useAuth()
+
+    if(!userId) {
+      router.push("/sign-in")
+    }
 
     const [data , setData] = useState<Blogs | undefined>(undefined)
     const [loading , setLoading] = useState<boolean>(false)
@@ -35,6 +46,23 @@ const BlogPage : React.FC = () => {
       fetchBlogById(id)
     } , [id])
 
+    const onDelete = async () =>  {
+      try {
+          setLoading(true)
+          await axios.delete(`/api/blog/${data?._id}`)
+
+          router.push("/")
+          toast.success("Delete successful")
+          router.refresh()
+
+      } catch (error) {
+          toast.error(" Deletion Failed")
+          console.log("Error : " , error)
+      } finally {
+        setLoading(false)
+      }
+    }
+
 
   return (
     <div className='w-full flex items-center justify-center'>
@@ -46,7 +74,26 @@ const BlogPage : React.FC = () => {
                   
               <div className="flex flex-col justify-center items-center leading-[2.5rem] w-full p-3 border-b border-stone-200 mb-4 gap-3">
                 <h1 className='md:text-[2.5rem] text-[1.4rem] font-semibold tracking-tighter leading-6 md:leading-none'>{data.title}</h1>
-                <p className="text-sm text-gray-500">{formatDate(data.createdAt)}</p>
+                
+                <div className="flex flex-col justify-center items-center gap-y-2">
+                  <p className="text-sm text-gray-500">{formatDate(data.createdAt)}</p>
+
+                  { 
+                    userId === data.authorId && 
+                    <div className="flex justify-center items-center gap-2">
+                    
+                      <button onClick={onDelete} className='flex items-center justify-center gap-1 py-1 px-2 bg-red-600 rounded-md text-white cursor-pointer text-[0.9rem] md:text-sm'>
+                        <span className='hidden md:block'>Delete</span> <Trash className='md:w-4 md:h-4 w-[1.35rem] h-[1.35rem]' />
+                      </button>
+
+                      <button onClick={() => router.push('/write')} className='flex items-center justify-center gap-1 py-1 px-2 bg-black hover:bg-gray-600 rounded-md text-white cursor-pointer text-[0.9rem] md:text-sm'>
+                        <span className='hidden md:block'>Edit</span> <Edit className='md:w-4 md:h-4 w-[1.35rem] h-[1.35rem]' />
+                      </button>
+                    </div>
+                  }
+
+                </div>
+
               </div>
   
               <div className="relative md:w-[55rem] md:h-[30rem] w-[23rem] h-[20rem] rounded-[2rem] overflow-hidden">

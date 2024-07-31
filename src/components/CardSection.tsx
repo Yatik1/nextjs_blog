c73ac5d1513d8daba0e3c7a89a1e663c759e4de8
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-07-31 13:38:31 +0530

    adding some conditions for animations

--- src/components/CardSection.tsx
+++ src/components/CardSection.tsx
@@ -9,7 +9,8 @@ const CardSection = ({blogs} : {blogs : Blogs[]}) => {
 
     return (
         <div className="flex items-start pl-7 pb-[3rem]">
-            <motion.div 
+            {blogs.length > 3 ? (
+                <motion.div 
                 initial={{ x: 0 }}
                 animate={{ x: "-50%" }}
                 transition={{ repeat: Infinity, ease: "linear", duration:30, delay:0.5 }}
@@ -31,7 +32,19 @@ const CardSection = ({blogs} : {blogs : Blogs[]}) => {
                         index={index}
                     />  
                 ))}
-            </motion.div>   
+            </motion.div> 
+            ) : (
+                <div className='gap-[1.3rem] items-start hidden md:flex'>
+                {blogs.map((data,index) => (
+                    <Card 
+                        id={data._id}
+                        title={data.title}
+                        cover={data.coverImg}
+                        index={index}
+                    />  
+                ))}
+                </div>
+            ) }
         </div>
     )
 }

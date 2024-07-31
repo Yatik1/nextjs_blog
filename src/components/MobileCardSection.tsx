accb762a4b68da98befc9ca82aa870de3605c0a2
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-07-31 14:05:45 +0530

    bug fixed 2

--- src/components/MobileCardSection.tsx
+++ src/components/MobileCardSection.tsx
@@ -9,7 +9,7 @@ const MobileCardSection = ({blogs} : {blogs : Blogs[]}) => {
     <div className="h-fit w-screen md:hidden">
         <div className='flex flex-col gap-2 justify-start items-center mb-3'>
         {blogs.map((data,index) => (
-            <Link href={`/blog/${data._id}`}>
+            <Link href={`/blog/${data._id}`} key={index}>
               <Card
               key={index}
               id={data._id}

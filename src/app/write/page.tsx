38b6502dc4b08f96ecd135248e3268402079c7df
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-07-31 13:38:09 +0530

    write form

--- src/app/write/page.tsx
+++ src/app/write/page.tsx
@@ -13,7 +13,7 @@ const Blog = () => {
   return (
     <div className="flex items-center justify-center overflow-x-hidden">
         <div className="flex flex-col items-start justify-start md:w-11/12 md:p-7 p-[4rem] gap-10 md:gap-0">
-            <h1 className="font-bold text-[2rem] md:text-[4rem] w-screen flex items-center justify-center">Write your blog</h1>
+            <h1 className="font-bold text-[2rem] md:text-[4rem] w-full flex items-center justify-center">Write your blog</h1>
             <FormEditor userId={userId} />
         </div>
     </div>

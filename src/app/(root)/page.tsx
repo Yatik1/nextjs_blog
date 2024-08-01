3181f02c5c528840cace9a5ed9784103475a552c
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-08-02 00:06:27 +0530

    mount -unmount

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,22 +1,24 @@
-"use client"
+'use client';
 
 import CardSection from "@/components/CardSection"
 import useBlog from "../hooks/useBlogModal"
-import { useEffect} from "react"
+import { useEffect, useState} from "react"
 import MobileCardSection from "@/components/MobileCardSection"
-import toast from "react-hot-toast"
 
 const HomePage = () => {
 
+  const [isMounted, setIsMounted] = useState(false)
   const {blogs , loading , fetchBlogs} = useBlog()
 
+  useEffect(() => {
+    setIsMounted(true)
+  } , [])
 
   useEffect(() => {
-    fetchBlogs().catch(error => {
-      toast.error("Error Occurred !")
-      console.log(error)
-    }) 
+    fetchBlogs()
   } , [fetchBlogs])
+
+  if(!isMounted) return null;
 
 
   if (loading) {

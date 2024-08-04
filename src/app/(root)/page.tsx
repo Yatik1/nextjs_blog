89785823e3ae4d79ff7af6425081d62199ec2059
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-08-04 12:18:42 +0530

    debug detected

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,17 +1,40 @@
 "use client";
 
 import CardSection from "@/components/CardSection"
-import useBlog from "../hooks/useBlogModal"
 import { useEffect, useState} from "react"
 import MobileCardSection from "@/components/MobileCardSection"
+import { Blogs } from "@/types/types";
+import axios from "axios";
 
 const HomePage = () => {
 
   const [isMounted, setIsMounted] = useState(false)
-  const {blogs , loading , error} = useBlog()
+
+  const [data , setData] = useState<Blogs[] | undefined>(undefined)
+  const [loading , setLoading] = useState<boolean>(false)
 
   useEffect(() => {
     setIsMounted(true)
+  } , [])
+
+
+  useEffect(() => {
+    const fetchBlog = async () => {
+        try {
+          
+          setLoading(true)
+
+          const response = await axios.get<Blogs[]>(`/api/`)
+          setData(response.data)
+
+        } catch (error) {
+          console.log(error)
+        } finally {
+          setLoading(false)
+        }
+    }
+
+    fetchBlog()
   } , [])
 
 
@@ -22,7 +45,7 @@ const HomePage = () => {
     return <p className="flex w-full h-screen items-center justify-center">Loading ..... </p>
   }
 
-  if(!loading && blogs.length === 0) {
+  if(!loading && data?.length === 0) {
     <p className='mt-[10rem] flex justify-center items-start text-slate-500'>No Blogs Available. ❌</p>
   }
 
@@ -33,8 +56,8 @@ const HomePage = () => {
         <h1 className="font-bold italic text-[3rem] pt-[1.2rem]">All Blogs {`->`}</h1>
       </div>
 
-      <CardSection blogs = {blogs}/>
-      <MobileCardSection blogs={blogs}/>
+      <CardSection blogs = {data}/>
+      <MobileCardSection blogs={data}/>
 
     </div>
       

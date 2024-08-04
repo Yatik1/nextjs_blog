e5bc002f84c1dc8fe637d3ac0c4ba5eb301342dd
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-08-04 12:32:55 +0530

    structuring

---
+++ src/app/(routes)/write/[id]/page.tsx
@@ -0,0 +1,53 @@
+"use client"
+
+import FormEditor from '@/components/ui/FormEditor'
+import { Blogs } from '@/types/types'
+import { useAuth } from '@clerk/nextjs'
+import axios from 'axios'
+import { redirect, useParams } from 'next/navigation'
+import { useEffect, useState } from 'react'
+
+const EditBlog = () => {
+
+    const [data, setData] = useState<Blogs | undefined>(undefined)
+    const [loading , setLoading] = useState<boolean>(false)
+
+    const params = useParams()
+    const {userId} = useAuth()
+
+    const {id} = params as {id :string}
+
+    if(!userId) {
+      redirect("/sign-in")
+    }
+
+
+    useEffect(() => {
+      (async () => {
+        const fetchBlogById = async (id: string) => {
+          try {
+            setLoading(true);
+            const response = await axios.get<Blogs>(`/api/blog/${id}`);
+            setData(response.data);
+          } catch (error) {
+            console.log(error);
+          } finally {
+            setLoading(false);
+          }
+        };
+    
+        fetchBlogById(id);
+      })();
+    }, [id]);
+  
+    return (
+      <div className="flex items-center justify-center overflow-x-hidden">
+          <div className="flex flex-col items-start justify-start md:w-11/12 md:p-7 p-[4rem] gap-10 md:gap-0">
+              <h1 className="font-bold text-[2rem] md:text-[4rem] w-full flex items-center justify-center">Edit your blog</h1>
+              <FormEditor userId={userId} data={data} />
+          </div>
+      </div>
+    )
+}
+
+export default EditBlog
---
+++ src/app/(routes)/write/page.tsx
@@ -0,0 +1,23 @@
+import FormEditor from "@/components/ui/FormEditor"
+import { auth } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation"
+
+const Blog = () => {
+
+  const {userId} = auth()
+
+  if(!userId) {
+    redirect("/sign-in")
+  } 
+
+  return (
+    <div className="flex items-center justify-center overflow-x-hidden">
+        <div className="flex flex-col items-start justify-start md:w-11/12 md:p-7 p-[4rem] gap-10 md:gap-0">
+            <h1 className="font-bold text-[2rem] md:text-[4rem] w-full flex items-center justify-center">Write your blog</h1>
+            <FormEditor userId={userId} />
+        </div>
+    </div>
+  )
+}
+
+export default Blog

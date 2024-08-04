ea95c0460ecccd9464f847837937673858b0fa4f
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-08-04 12:18:22 +0530

    no-use

--- src/app/hooks/useBlogModal.ts
+++ src/app/hooks/useBlogModal.ts
@@ -1,19 +1,19 @@
-import useSWR from 'swr';
+// import useSWR from 'swr';
 
-export const revalidate=0;
+// export const revalidate=0;
 
-const fetcher = (url : string) => fetch(`${url}?timestamp=${new Date().getTime()}`, { cache: 'no-store' }).then((res) => res.json());
+// const fetcher = (url : string) => fetch(url, { cache: 'no-store' }).then((res) => res.json());
 
-const useBlog = () => {
-  const { data: blogs, error } = useSWR('/api/', fetcher, { revalidateOnFocus: true , refreshInterval : 2000 });
+// const useBlog = () => {
+//   const { data: blogs, error } = useSWR('/api/', fetcher, { revalidateOnFocus: true });
 
-  const loading = !error && !blogs;
+//   const loading = !error && !blogs;
 
-  return {
-    blogs: blogs || [],
-    loading,
-    error,
-  };
-};
+//   return {
+//     blogs: blogs || [],
+//     loading,
+//     error,
+//   };
+// };
 
-export default useBlog;
+// export default useBlog;

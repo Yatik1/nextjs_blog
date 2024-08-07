4b51145190b14fff1aaa95ae1cb8ac3fd875bfd4
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-08-07 12:01:28 +0530

    route update

--- src/app/api/blog/(root)/route.ts
+++ src/app/api/blog/(root)/route.ts
@@ -2,13 +2,21 @@ import dbConnect from "@/lib/dbConnect";
 import BlogModel from "@/model/Blog";
 import { NextResponse } from "next/server";
 
-export async function GET (req:Request) {
+export async function POST (req:Request) {
 
     try {
         await dbConnect()
         
         const blogs = await BlogModel.find()
-        return NextResponse.json(blogs);
+
+        const headers = new Headers();
+        headers.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+        headers.set('Pragma', 'no-cache');
+        headers.set('Expires', '0');
+        headers.set('Surrogate-Control', 'no-store');
+
+
+        return NextResponse.json(blogs , {headers});
 
     } catch (error) {
         console.log("[GET ALL ERROR]" , error);
@@ -16,3 +24,4 @@ export async function GET (req:Request) {
         
     }
 }
+

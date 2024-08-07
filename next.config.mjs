b8fd37010a3554f986b0291f513f1b1f18e8c8d5
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-08-07 12:01:18 +0530

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,29 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async headers() {
-        return [
-          {
-            source: "/api/(.*)",
-            headers: [
-              {
-                key: "Cache-Control",
-                value: "no-store, no-cache, must-revalidate, proxy-revalidate",
-              },
-              {
-                key: "Pragma",
-                value: "no-cache",
-              },
-              {
-                key: "Expires",
-                value: "0",
-              },
-            ],
-          },
-        ];
-      },
       images: {
         domains: ['res.cloudinary.com'],
       },
+      logging : {
+        fetches : {
+          fullUrl : true,
+        }
+      }
 };
 
 export default nextConfig;
